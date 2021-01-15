e2e653c61838c657bd49ad0a85811f2bab440ddb
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2021-01-15 13:24:44 +0100

    changed wxFormBuilder files encoding to UTF BOM

--- src/gui/widgets/vehicle_creator_dialog/interface/i_vehicle_creator_dialog.cpp
+++ src/gui/widgets/vehicle_creator_dialog/interface/i_vehicle_creator_dialog.cpp
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////
+﻿///////////////////////////////////////////////////////////////////////////
 // C++ code generated with wxFormBuilder (version Oct 26 2018)
 // http://www.wxformbuilder.org/
 //
@@ -11,7 +11,8 @@
 
 IVehicleCreatorDialog::IVehicleCreatorDialog( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
 {
-	this->SetSizeHints( wxSize( -1,800 ), wxDefaultSize );
+	this->SetSizeHints( wxSize( 550,800 ), wxDefaultSize );
+	this->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, wxEmptyString ) );
 
 	wxBoxSizer* bSizer1;
 	bSizer1 = new wxBoxSizer( wxVERTICAL );
@@ -21,24 +22,24 @@ IVehicleCreatorDialog::IVehicleCreatorDialog( wxWindow* parent, wxWindowID id, c
 	fgSizer1->SetFlexibleDirection( wxBOTH );
 	fgSizer1->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
 
-	m_staticText_vehicleType = new wxStaticText( this, wxID_ANY, wxT("Rodzaj pojazdu"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_vehicleType = new wxStaticText( this, wxID_ANY, _("Rodzaj pojazdu"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_vehicleType->Wrap( -1 );
 	fgSizer1->Add( m_staticText_vehicleType, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
-	wxString m_choice_vehicleTypeChoices[] = { wxT("Samochód osobowy"), wxT("Motocykl"), wxT("SUV"), wxT("VAN"), wxT("Ciężarówka"), wxEmptyString };
+	wxString m_choice_vehicleTypeChoices[] = { _("Samochód osobowy"), _("Motocykl"), _("SUV"), _("VAN"), _("Ciężarówka"), wxEmptyString };
 	int m_choice_vehicleTypeNChoices = sizeof( m_choice_vehicleTypeChoices ) / sizeof( wxString );
 	m_choice_vehicleType = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxSize( 280,-1 ), m_choice_vehicleTypeNChoices, m_choice_vehicleTypeChoices, 0 );
 	m_choice_vehicleType->SetSelection( 0 );
 	fgSizer1->Add( m_choice_vehicleType, 0, wxALL, 5 );
 
-	m_staticText_brand = new wxStaticText( this, wxID_ANY, wxT("Marka"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_brand = new wxStaticText( this, wxID_ANY, _("Marka"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_brand->Wrap( -1 );
 	fgSizer1->Add( m_staticText_brand, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_brand = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( -1,-1 ), 0 );
 	fgSizer1->Add( m_textCtrl_brand, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_model = new wxStaticText( this, wxID_ANY, wxT("Model"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_model = new wxStaticText( this, wxID_ANY, _("Model"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_model->Wrap( -1 );
 	fgSizer1->Add( m_staticText_model, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
@@ -51,55 +52,55 @@ IVehicleCreatorDialog::IVehicleCreatorDialog( wxWindow* parent, wxWindowID id, c
 
 	fgSizer1->Add( 0, 0, 1, wxEXPAND, 5 );
 
-	m_staticText_fuelType = new wxStaticText( this, wxID_ANY, wxT("Rodzaj paliwa"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_fuelType = new wxStaticText( this, wxID_ANY, _("Rodzaj paliwa"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_fuelType->Wrap( -1 );
 	fgSizer1->Add( m_staticText_fuelType, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
-	wxString m_choice_fuelTypeChoices[] = { wxT("Benzyna"), wxT("Diesel"), wxT("LPG"), wxT("Elektryk") };
+	wxString m_choice_fuelTypeChoices[] = { _("Benzyna"), _("Diesel"), _("LPG"), _("Elektryk") };
 	int m_choice_fuelTypeNChoices = sizeof( m_choice_fuelTypeChoices ) / sizeof( wxString );
 	m_choice_fuelType = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choice_fuelTypeNChoices, m_choice_fuelTypeChoices, 0 );
 	m_choice_fuelType->SetSelection( 0 );
 	fgSizer1->Add( m_choice_fuelType, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_transmission = new wxStaticText( this, wxID_ANY, wxT("Rodzaj skrzyni biegów"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_transmission = new wxStaticText( this, wxID_ANY, _("Rodzaj skrzyni biegów"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_transmission->Wrap( -1 );
 	fgSizer1->Add( m_staticText_transmission, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
-	wxString m_choice_transmissionChoices[] = { wxT("Manualna"), wxT("Automatyczna") };
+	wxString m_choice_transmissionChoices[] = { _("Manualna"), _("Automatyczna") };
 	int m_choice_transmissionNChoices = sizeof( m_choice_transmissionChoices ) / sizeof( wxString );
 	m_choice_transmission = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxSize( -1,-1 ), m_choice_transmissionNChoices, m_choice_transmissionChoices, 0 );
 	m_choice_transmission->SetSelection( 0 );
 	fgSizer1->Add( m_choice_transmission, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_wheelDrive = new wxStaticText( this, wxID_ANY, wxT("Rodzaj napędu"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_wheelDrive = new wxStaticText( this, wxID_ANY, _("Rodzaj napędu"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_wheelDrive->Wrap( -1 );
 	fgSizer1->Add( m_staticText_wheelDrive, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
-	wxString m_choice_wheelDriveChoices[] = { wxT("Na przednie koła"), wxT("Na tylnie koła"), wxT("4x4") };
+	wxString m_choice_wheelDriveChoices[] = { _("Na przednie koła"), _("Na tylnie koła"), _("4x4") };
 	int m_choice_wheelDriveNChoices = sizeof( m_choice_wheelDriveChoices ) / sizeof( wxString );
 	m_choice_wheelDrive = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxSize( -1,-1 ), m_choice_wheelDriveNChoices, m_choice_wheelDriveChoices, 0 );
 	m_choice_wheelDrive->SetSelection( 1 );
 	fgSizer1->Add( m_choice_wheelDrive, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_engineCapacity = new wxStaticText( this, wxID_ANY, wxT("Pojemność silnika [L]"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineCapacity = new wxStaticText( this, wxID_ANY, _("Pojemność silnika [L]"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_engineCapacity->Wrap( -1 );
 	fgSizer1->Add( m_staticText_engineCapacity, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_engineCapacity = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( -1,-1 ), 0 );
 	fgSizer1->Add( m_textCtrl_engineCapacity, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_engineHorsepower = new wxStaticText( this, wxID_ANY, wxT("Moc silnika [KM]"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineHorsepower = new wxStaticText( this, wxID_ANY, _("Moc silnika [KM]"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_engineHorsepower->Wrap( -1 );
 	fgSizer1->Add( m_staticText_engineHorsepower, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_engineHorsepower = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( -1,-1 ), 0 );
 	fgSizer1->Add( m_textCtrl_engineHorsepower, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_doorQuantity = new wxStaticText( this, wxID_ANY, wxT("Ilość drzwi"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_doorQuantity = new wxStaticText( this, wxID_ANY, _("Ilość drzwi"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_doorQuantity->Wrap( -1 );
 	fgSizer1->Add( m_staticText_doorQuantity, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
-	wxString m_choice_doorQuantityChoices[] = { wxT("3"), wxT("5"), wxT("brak") };
+	wxString m_choice_doorQuantityChoices[] = { _("3"), _("5"), _("brak") };
 	int m_choice_doorQuantityNChoices = sizeof( m_choice_doorQuantityChoices ) / sizeof( wxString );
 	m_choice_doorQuantity = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxSize( -1,-1 ), m_choice_doorQuantityNChoices, m_choice_doorQuantityChoices, 0 );
 	m_choice_doorQuantity->SetSelection( 0 );
@@ -111,35 +112,35 @@ IVehicleCreatorDialog::IVehicleCreatorDialog( wxWindow* parent, wxWindowID id, c
 
 	fgSizer1->Add( 0, 0, 1, wxEXPAND, 5 );
 
-	m_staticText_price = new wxStaticText( this, wxID_ANY, wxT("Cena [zł]"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_price = new wxStaticText( this, wxID_ANY, _("Cena [zł]"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_price->Wrap( -1 );
 	fgSizer1->Add( m_staticText_price, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_price = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizer1->Add( m_textCtrl_price, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_mileage = new wxStaticText( this, wxID_ANY, wxT("Przebieg [km]"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_mileage = new wxStaticText( this, wxID_ANY, _("Przebieg [km]"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_mileage->Wrap( -1 );
 	fgSizer1->Add( m_staticText_mileage, 0, wxALL|wxALIGN_CENTER_VERTICAL|wxALIGN_RIGHT, 5 );
 
 	m_textCtrl_mileage = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizer1->Add( m_textCtrl_mileage, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_color = new wxStaticText( this, wxID_ANY, wxT("Kolor"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_color = new wxStaticText( this, wxID_ANY, _("Kolor"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_color->Wrap( -1 );
 	fgSizer1->Add( m_staticText_color, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_color = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizer1->Add( m_textCtrl_color, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_state = new wxStaticText( this, wxID_ANY, wxT("Stan techniczny"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_state = new wxStaticText( this, wxID_ANY, _("Stan techniczny"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_state->Wrap( -1 );
 	fgSizer1->Add( m_staticText_state, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
 	m_textCtrl_state = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizer1->Add( m_textCtrl_state, 0, wxALL|wxEXPAND, 5 );
 
-	m_staticText_additInfo = new wxStaticText( this, wxID_ANY, wxT("Dodatkowe informacje"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_additInfo = new wxStaticText( this, wxID_ANY, _("Dodatkowe informacje"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText_additInfo->Wrap( -1 );
 	fgSizer1->Add( m_staticText_additInfo, 0, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5 );
 
@@ -152,7 +153,7 @@ IVehicleCreatorDialog::IVehicleCreatorDialog( wxWindow* parent, wxWindowID id, c
 
 	bSizer1->Add( 0, 20, 0, wxEXPAND, 5 );
 
-	m_button_submitVehicle = new wxButton( this, wxID_ANY, wxT("Dodaj"), wxDefaultPosition, wxSize( 300,-1 ), 0 );
+	m_button_submitVehicle = new wxButton( this, wxID_ANY, _("Dodaj"), wxDefaultPosition, wxSize( 300,-1 ), 0 );
 	bSizer1->Add( m_button_submitVehicle, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5 );
 
 
