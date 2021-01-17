b053bba7d4bbe1c29ffab2112bdebb8677734fc1
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2021-01-17 14:54:34 +0100

    Add files via upload
    Added vehicle search dialog

---
+++ src/gui/widgets/vehicle_search_dialog/implementation/vehicle_search_dialog.cpp
@@ -0,0 +1,19 @@
+﻿#include "vehicle_search_dialog.h"
+
+SVehicleSearchDialog::SVehicleSearchDialog(CVehicleManager* database, wxWindow* parent)
+:
+IVehicleSearchDialog( parent )
+{
+
+}
+
+void SVehicleSearchDialog::OnSearchSubmitVehicleButtonClicked( wxCommandEvent& event )
+{
+	Close();
+}
+
+void SVehicleSearchDialog::OnCancel( wxCommandEvent& event )
+{
+	Close();
+}
+
---
+++ src/gui/widgets/vehicle_search_dialog/implementation/vehicle_search_dialog.h
@@ -0,0 +1,17 @@
+﻿#ifndef __vehicle_search_dialog__
+#define __vehicle_search_dialog__
+
+
+#include "../interface/i_vehicle_search_dialog.h"
+#include "database/vehicle_management/vehicle_manager.hpp"
+
+class SVehicleSearchDialog : public IVehicleSearchDialog
+{
+	protected:
+		void OnSearchSubmitVehicleButtonClicked( wxCommandEvent& event );
+		void OnCancel( wxCommandEvent& event );
+	public:
+		SVehicleSearchDialog(CVehicleManager* database, wxWindow* parent);
+};
+
+#endif 
