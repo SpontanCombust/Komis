05eb865c5ea928b8da27d2b4b5de1bee633e60f9
SpontanCombust <cedro_p@o2.pl>
2021-01-18 04:27:41 +0100

    Added vehicle list item panel implementation

---
+++ src/gui/widgets/scrolled_vehicle_list_item_panel/implementation/scrolled_vehicle_list_item_panel.cpp
@@ -0,0 +1,43 @@
+﻿#include "scrolled_vehicle_list_item_panel.hpp"
+
+CScrolledVehicleListItemPanel::CScrolledVehicleListItemPanel( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name ) 
+: IScrolledVehicleListItemPanel( parent, id, pos, size, style, name )
+{
+
+}
+
+CScrolledVehicleListItemPanel::~CScrolledVehicleListItemPanel() 
+{
+
+}
+
+
+void CScrolledVehicleListItemPanel::setBrandAndModel( std::string brand, std::string model ) 
+{
+    m_staticText_brandAndModel->SetLabelText( _( brand + " " + model ) );
+}
+
+void CScrolledVehicleListItemPanel::setState( std::string state ) 
+{
+    m_staticText_state->SetLabelText( _( state ) );
+}
+
+void CScrolledVehicleListItemPanel::setMileage(unsigned int mileage) 
+{
+    m_staticText_mileage->SetLabelText( _( std::to_string( mileage ) + " km" ) );
+}
+
+void CScrolledVehicleListItemPanel::setEngineHorsepower( unsigned int horsepower ) 
+{
+    m_staticText_engineHorsepower->SetLabelText( _( std::to_string( horsepower ) + " KM" ) );
+}
+
+void CScrolledVehicleListItemPanel::setEngineCapacity( unsigned int capacity ) 
+{
+    m_staticText_engineHorsepower->SetLabelText( _( std::to_string( capacity ) + " L" ) );
+}
+
+void CScrolledVehicleListItemPanel::setPrice( float price ) 
+{
+    m_staticText_engineHorsepower->SetLabelText( _( std::to_string( price ) + " zł" ) );
+}
---
+++ src/gui/widgets/scrolled_vehicle_list_item_panel/implementation/scrolled_vehicle_list_item_panel.hpp
@@ -0,0 +1,21 @@
+﻿#ifndef __SCROLLED_VEHICLE_LIST_ITEM_PANEL_H__
+#define __SCROLLED_VEHICLE_LIST_ITEM_PANEL_H__
+
+
+#include "../interface/i_scrolled_vehicle_list_item_panel.h"
+
+class CScrolledVehicleListItemPanel : public IScrolledVehicleListItemPanel
+{
+public:
+	CScrolledVehicleListItemPanel( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxTAB_TRAVERSAL, const wxString& name = wxEmptyString );
+	~CScrolledVehicleListItemPanel();
+
+	void setBrandAndModel( std::string brand, std::string model );
+	void setState( std::string state );
+	void setMileage( unsigned int mileage );
+	void setEngineHorsepower( unsigned int horsepower );
+	void setEngineCapacity( unsigned int capacity );
+	void setPrice( float price );
+};
+
+#endif // __SCROLLED_VEHICLE_LIST_ITEM_PANEL_H__
