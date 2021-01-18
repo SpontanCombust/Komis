5950bb902260d39c8ac0a3db61275f7a106cd2d3
SpontanCombust <cedro_p@o2.pl>
2021-01-18 04:10:35 +0100

    Added scrollable list item interface

---
+++ src/gui/widgets/scrolled_vehicle_list_item_panel/interface/i_scrolled_vehicle_list_item_panel.cpp
@@ -0,0 +1,119 @@
+﻿///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "i_scrolled_vehicle_list_item_panel.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+IScrolledVehicleListItemPanel::IScrolledVehicleListItemPanel( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name ) : wxPanel( parent, id, pos, size, style, name )
+{
+	wxBoxSizer* bSizer8;
+	bSizer8 = new wxBoxSizer( wxVERTICAL );
+
+	wxBoxSizer* bSizer9;
+	bSizer9 = new wxBoxSizer( wxVERTICAL );
+
+	m_staticText_brandAndModel = new wxStaticText( this, wxID_ANY, _("markaModelTekst"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_brandAndModel->Wrap( -1 );
+	m_staticText_brandAndModel->SetFont( wxFont( 20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer9->Add( m_staticText_brandAndModel, 4, wxALIGN_CENTER_VERTICAL|wxEXPAND|wxTOP|wxRIGHT|wxLEFT, 10 );
+
+
+	bSizer8->Add( bSizer9, 3, wxEXPAND, 5 );
+
+	wxBoxSizer* bSizer10;
+	bSizer10 = new wxBoxSizer( wxHORIZONTAL );
+
+	wxBoxSizer* bSizer11;
+	bSizer11 = new wxBoxSizer( wxHORIZONTAL );
+
+	m_staticText_stateLabel = new wxStaticText( this, wxID_ANY, _("Stan:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_stateLabel->Wrap( -1 );
+	bSizer11->Add( m_staticText_stateLabel, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+	m_staticText_state = new wxStaticText( this, wxID_ANY, _("stanTekst"), wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_END );
+	m_staticText_state->Wrap( -1 );
+	m_staticText_state->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer11->Add( m_staticText_state, 1, wxALIGN_CENTER_VERTICAL|wxALL, 0 );
+
+
+	bSizer10->Add( bSizer11, 1, wxALIGN_CENTER_VERTICAL|wxRIGHT|wxEXPAND, 5 );
+
+	wxBoxSizer* bSizer12;
+	bSizer12 = new wxBoxSizer( wxHORIZONTAL );
+
+	m_staticText_mileageLabel = new wxStaticText( this, wxID_ANY, _("Przebieg:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_mileageLabel->Wrap( -1 );
+	bSizer12->Add( m_staticText_mileageLabel, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+	m_staticText_mileage = new wxStaticText( this, wxID_ANY, _("przebiegTekst"), wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_END );
+	m_staticText_mileage->Wrap( -1 );
+	m_staticText_mileage->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer12->Add( m_staticText_mileage, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+
+	bSizer10->Add( bSizer12, 1, wxALIGN_CENTER_VERTICAL|wxRIGHT|wxLEFT|wxEXPAND, 5 );
+
+	wxBoxSizer* bSizer14;
+	bSizer14 = new wxBoxSizer( wxHORIZONTAL );
+
+	m_staticText_engineHorsepowerLabel = new wxStaticText( this, wxID_ANY, _("Moc silnika:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineHorsepowerLabel->Wrap( -1 );
+	bSizer14->Add( m_staticText_engineHorsepowerLabel, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+	m_staticText_engineHorsepower = new wxStaticText( this, wxID_ANY, _("mocTekst"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineHorsepower->Wrap( -1 );
+	m_staticText_engineHorsepower->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer14->Add( m_staticText_engineHorsepower, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+
+	bSizer10->Add( bSizer14, 1, wxEXPAND|wxLEFT|wxALIGN_CENTER_VERTICAL, 5 );
+
+	wxBoxSizer* bSizer13;
+	bSizer13 = new wxBoxSizer( wxHORIZONTAL );
+
+	m_staticText_engineCapacityLabel = new wxStaticText( this, wxID_ANY, _("Pojemność silnika:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineCapacityLabel->Wrap( -1 );
+	bSizer13->Add( m_staticText_engineCapacityLabel, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+	m_staticText_engineCapacity = new wxStaticText( this, wxID_ANY, _("pojemnośćTekst"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_engineCapacity->Wrap( -1 );
+	m_staticText_engineCapacity->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer13->Add( m_staticText_engineCapacity, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+
+	bSizer10->Add( bSizer13, 1, wxRIGHT|wxLEFT|wxALIGN_CENTER_VERTICAL|wxEXPAND, 5 );
+
+
+	bSizer8->Add( bSizer10, 2, wxALIGN_CENTER_HORIZONTAL, 5 );
+
+	wxBoxSizer* bSizer15;
+	bSizer15 = new wxBoxSizer( wxVERTICAL );
+
+	m_staticText_price = new wxStaticText( this, wxID_ANY, _("cenaTekst"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText_price->Wrap( -1 );
+	m_staticText_price->SetFont( wxFont( 30, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
+
+	bSizer15->Add( m_staticText_price, 1, wxALL|wxALIGN_CENTER_VERTICAL|wxEXPAND, 5 );
+
+
+	bSizer8->Add( bSizer15, 4, wxALIGN_CENTER_HORIZONTAL|wxEXPAND, 5 );
+
+
+	this->SetSizer( bSizer8 );
+	this->Layout();
+	bSizer8->Fit( this );
+}
+
+IScrolledVehicleListItemPanel::~IScrolledVehicleListItemPanel()
+{
+}
---
+++ src/gui/widgets/scrolled_vehicle_list_item_panel/interface/i_scrolled_vehicle_list_item_panel.h
@@ -0,0 +1,50 @@
+﻿///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/intl.h>
+#include <wx/string.h>
+#include <wx/stattext.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/sizer.h>
+#include <wx/panel.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class IScrolledVehicleListItemPanel
+///////////////////////////////////////////////////////////////////////////////
+class IScrolledVehicleListItemPanel : public wxPanel
+{
+	private:
+
+	protected:
+		wxStaticText* m_staticText_brandAndModel;
+		wxStaticText* m_staticText_stateLabel;
+		wxStaticText* m_staticText_state;
+		wxStaticText* m_staticText_mileageLabel;
+		wxStaticText* m_staticText_mileage;
+		wxStaticText* m_staticText_engineHorsepowerLabel;
+		wxStaticText* m_staticText_engineHorsepower;
+		wxStaticText* m_staticText_engineCapacityLabel;
+		wxStaticText* m_staticText_engineCapacity;
+		wxStaticText* m_staticText_price;
+
+	public:
+
+		IScrolledVehicleListItemPanel( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxTAB_TRAVERSAL, const wxString& name = wxEmptyString );
+		~IScrolledVehicleListItemPanel();
+
+};
+
